c61fb9a6273cb51ae40421945c282ecf4a0f8c87
Simran757 <simrankashyap757@gmail.com>
2024-12-23 19:44:51 +0530

    first project

---
+++ src/main/java/com/basic/project_1_form_based_authentication/Project1FormBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.basic.project_1_form_based_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1FormBasedAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1FormBasedAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/project_1_form_based_authentication/Project1FormBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.project_1_form_based_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1FormBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
